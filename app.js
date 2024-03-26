a266148100f0542f31b17e68f77ec3c922d4b5d9
Jeffrey Doyle <doylejeff98@gmail.com>
2024-03-26 13:47:09 -0400

    ejs layout

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const expressLayout = require('express-ejs-layouts')
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
