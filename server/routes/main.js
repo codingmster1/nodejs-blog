07c3f0cce8cb9bc942ebbb12ecbb3194899649c6
Jeffrey Doyle <doylejeff98@gmail.com>
2024-03-29 13:24:38 -0400

    fixed mongoose errors for functions

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,13 +3,38 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 //routes
-router.get ('', (req, res) => {
+router.get ('', async (req, res) => {
+    try {
+        const locals = {
+            title: "Cat's Name Blog",
+            description: "Blog created in nodeJs and mongodb"
+        }
 
-    const locals = {
-        title: "Cat's Name Blog",
-        description: "Blog created in nodeJs and mongodb"
+let perPage = 10;
+let page = req.query.page || 1;
+
+const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+.skip(perPage * page - perPage)
+.limit(perPage)
+.exec();
+
+const count = await Post.countDocuments({});
+const nextPage = parseInt(page) + 1;
+const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+//const data = await Post.find();
+res.render('index', { 
+    locals, 
+    data,
+    current: page, 
+    nextPage: hasNextPage ? nextPage : null
+});
+    
+
+} catch (error) {
+console.log(error);
     }
-    res.render('index', { locals });
+    
 });
 
 
